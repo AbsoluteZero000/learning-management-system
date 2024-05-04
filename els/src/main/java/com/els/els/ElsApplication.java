4d40fcffe1f215b688054cc8420a897d5a2408c0
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-05-04 12:15:33 +0300

    inital app

---
+++ els/src/main/java/com/els/els/ElsApplication.java
@@ -0,0 +1,13 @@
+package com.els.els;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsApplication.class, args);
+	}
+
+}
---
+++ els/src/test/java/com/els/els/ElsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.els.els;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
