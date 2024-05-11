8b1ce7f8c910effba3aafc790ad3e1d3ba0e12d8
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-05-11 15:22:41 +0300

    smth

--- server/src/main/java/com/els/models/Account.java
+++ server/src/main/java/com/els/models/Account.java
@@ -1,23 +1,30 @@
 package com.els.models;
 
+import com.els.util.SignupWrapper;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy=InheritanceType.JOINED)
 @Table(name = "accounts")
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private String id;
+    private Integer id;
 
     @Column(name = "name")
     private String name;
@@ -31,4 +38,14 @@ public class Account {
     @Column(name = "role")
     private String role;
 
+    public Account(SignupWrapper wrapper){
+        this.name = wrapper.name;
+        this.email = wrapper.email;
+        this.password = wrapper.password;
+        this.role = wrapper.role;
+    }
+
+    public Account getAccount() {
+        return this;
+    }
 }
---
+++ server/src/main/java/com/els/util/LoginWrapper.java
@@ -0,0 +1,10 @@
+package com.els.util;
+
+
+
+public class LoginWrapper {
+    public String email;
+    public String name;
+    public String password;
+    public String role;
+}
